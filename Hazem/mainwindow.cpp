fef2f23291ae34f6cbdc86f1befffa924833b96b
Hazem Mostafa <36373104+Zoma17@users.noreply.github.com>
2018-10-28 01:29:03 +0200

    Add files via upload

---
+++ Hazem/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+
+    countd= new Countdown(this);
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
