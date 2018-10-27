1c072513c31c023b0f31ed0d1e1febb36be6275f
Hazem Mostafa <36373104+Zoma17@users.noreply.github.com>
2018-10-27 21:29:01 +0200

    Add files via upload

---
+++ Hazem/joystick.cpp
@@ -0,0 +1,44 @@
+#include "joystick.h"
+
+Joystick::Joystick()
+{
+
+    SDL_Init(SDL_INIT_JOYSTICK);
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+     if (SDL_INIT_JOYSTICK<0)
+         qDebug()<<"failed to init";
+     SDL_JoystickEventState(SDL_ENABLE);
+
+
+     if (SDL_JoystickGetAttached(js)){
+         qDebug() <<"attached";
+     qDebug() <<"Num of buttons is : "<<SDL_JoystickNumButtons(js);
+     qDebug()<<"Num of hats is : " <<SDL_JoystickNumHats(js);
+     qDebug() <<"num of axes is :" <<SDL_JoystickNumAxes(js);
+     qDebug() <<"num of balls is :" <<SDL_JoystickNumBalls(js); }
+}
+
+int Joystick::get_x()
+{
+    return SDL_JoystickGetAxis(js,0);
+}
+
+int Joystick::get_y()
+{
+    return -SDL_JoystickGetAxis(js,1);
+}
+int Joystick::get_z()
+{
+    return SDL_JoystickGetAxis(js,3);
+}
+int Joystick::get_r()
+{
+    return SDL_JoystickGetAxis(js,2);
+}
+
+void Joystick::activate()
+{
+    js= SDL_JoystickOpen(0);
+    qDebug()<< "joystick plugged";
+}
---
+++ Hazem/joystick_handler.cpp
@@ -0,0 +1,46 @@
+#include "joystick_handler.h"
+
+#define X js->get_x()
+#define Y js->get_y()
+#define Z js->get_z()
+#define R js->get_r()
+#define DEADZONE 4000
+#define SGNFCNT 300
+
+Joystick_Handler::Joystick_Handler()
+{
+    js=new Joystick;
+    prev_x=prev_r=prev_y=prev_z=0;
+    timer =new QTimer;
+    timer->start();
+    timer->setInterval(50);
+    connect(timer,SIGNAL(timeout()),this,SLOT(action()));
+
+
+}
+
+
+
+void Joystick_Handler::action()
+{
+    while (SDL_PollEvent(&event)){
+    switch (event.type){
+    case SDL_JOYAXISMOTION:
+        if (abs(X-prev_x)>SGNFCNT || abs(Y-prev_y)>SGNFCNT || abs(Z-prev_z)>SGNFCNT || abs(R-prev_r)>SGNFCNT){
+            msg="";
+            msg+=((abs(X)>DEADZONE)?to_string(X):"0") +" ";
+            msg+=((abs(Y)>DEADZONE)?to_string(Y):"0") +" ";
+            msg+=((abs(Z)>DEADZONE)?to_string(Z):"0") +" ";
+            msg+=(abs(R)>DEADZONE)?to_string(R):"0";
+            emit msgsent(msg);
+        }
+        break;
+    case SDL_JOYDEVICEADDED:
+        js->activate();
+        break;
+    default:
+        break;
+                       }
+
+                               }
+}
