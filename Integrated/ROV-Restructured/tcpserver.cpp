b3f95da64df52d041c6825d8cd85882a3f2ec9e3
abdelrahman <abdelrahman.bishr@icloud.com>
2019-02-28 14:01:48 +0200

    try & catch added to tcp server to watch for errors , not teset yet

--- Integrated/ROV-Restructured/tcpserver.cpp
+++ Integrated/ROV-Restructured/tcpserver.cpp
@@ -25,20 +25,18 @@ bool TCPServer::connectToServer()
     } catch (std::exception e) {
 
     }
-
-    //    qDebug()<<"Connected";
     return socket->waitForConnected(150);
 }
 
 void TCPServer::reconnect()
 {
-//    qDebug()<<reConTimer->isActive();
 
     try {
         socket->connectToHost(host.c_str(),port);
     } catch (std::exception e) {
-
+        qDebug()<<"TCP exception at line 40 , connection to host fails "<<e.what();
     }
+    try {
         if (socket->waitForConnected(150)){
             reConTimer->stop();
             connectionFlag=1;
@@ -46,35 +44,47 @@ void TCPServer::reconnect()
         else
             reConTimer->start();
 
+    } catch (std::exception e) {
+        //probably if this happens it is because of a low duration of waiting to reconnect , we shall increase it and see what happens
+        qDebug()<<"TCP exception at line 51 , waiting for getting connected to host throws exception "<<e.what();
+    }
+
+
 }
-
-//void TCPServer::sendmsg(QString message)
-//{
-
-//        qDebug()<<message;
-
-//        socket->write(message.toStdString().c_str());
-//}
 
 void TCPServer::read()
 {
-    string s=socket->readAll().toStdString();
-    emit receivedmsg(QString::fromStdString(s));
-    qDebug()<<"received from client :::"<<QString::fromStdString(s);
+    try {
+        string s=socket->readAll().toStdString();
+        emit receivedmsg(QString::fromStdString(s));
+        qDebug()<<"received from client :::"<<QString::fromStdString(s);
+
+    } catch (std::exception e) {
+        qDebug()<<"TCP exception at line 73 , reading received message grom server throws exception"<<e.what();
+    }
 
 }
 
 bool TCPServer::isConnected()
 {
-    return socket->state() == QAbstractSocket::ConnectedState;
+    try {
+        return socket->state() == QAbstractSocket::ConnectedState;
+    } catch (std::exception e) {
+        qDebug()<<"TCP exception at line 83 , checking state of socket throws exception "<<e.what();
+        return false;       //will tell : no connection if an exception happens , don't yet know if this is right or wrong , whether to return false or true
+    }
 }
 
 bool TCPServer::sendToServer(QString message)
 {
     if(isConnected()){
-        socket->write(message.toUtf8());
-        socket->flush();
-        return true;
+        try {
+            socket->write(message.toUtf8());
+            socket->flush();
+            return true;
+        } catch (std::exception e) {
+            qDebug()<<"TCP exception at line 96 , writting and flushing on a socket throw an exception "<<e.what();
+        }
     }
     return false;
 }
@@ -82,7 +92,6 @@ bool TCPServer::sendToServer(QString message)
 void TCPServer::sendMessage(QString message)
 
 {
-    try{
         if(/*socket->*/isConnected()){
             qDebug() << message;
             /*socket->*/sendToServer(message);
@@ -94,15 +103,17 @@ void TCPServer::sendMessage(QString message)
                 this->sendToServer(message);
             }
         }
-    }
-    catch(std::exception e){
-
-    }
 }
 
 void TCPServer::socketDisconnected()
 {
-    connectionFlag=0;
-    reconnect();
+    try {
+        if (socket->waitForDisconnected(150)){
+            connectionFlag=0;
+            reconnect();
+        }
+    } catch (std::exception e) {
+        qDebug()<<"TCP exception at line 116 , socket disconnected throw an exception "<<e.what();
+    }
 }
 
